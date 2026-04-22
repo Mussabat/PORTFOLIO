f52a45e216602b2b2330c40073d7616776a147c4
Mussabat <tnafisa18@gmail.com>
2026-04-22 15:51:10 +0100

    chore: scrool error fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,10 +21,6 @@ export const Hero: React.FC = () => {
         show: { opacity: 1, y: 0, transition: { duration: 0.6, ease: [0.25, 0.1, 0.25, 1] } },
     };
 
-    const scrollToWork = () => {
-        document.getElementById('work')?.scrollIntoView({ behavior: 'smooth' });
-    };
-
     return (
         <section
             id="top"
